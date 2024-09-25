576c4ee9516be2d63d05f377eb62e2776be82885
Damien <DamienB2474@protonmail.com>
2024-09-25 01:26:52 -0400

    slight sleep form changes

--- frontend/src/components/sleepForm/sleepForm.jsx
+++ frontend/src/components/sleepForm/sleepForm.jsx
@@ -3,13 +3,35 @@ import { Form, Button, Container, Modal, Col, Row } from "react-bootstrap";
 
 const SleepForm = () => {
   const [show, setShow] = useState(false);
-  
+  const [error, setError] = useState(null);
+  // add state changes for form
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const handleSubmit = async (e) => {
     e.preventDefault();
-  }
+
+    // set as sleep data for post request
+    const sleepData = {};
+
+    const responce = await fetch("/sleepItem", {
+      method: "POST",
+      body: JSON.stringify(sleepData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const json = await responce.json();
+    if (!responce.ok) {
+      setError(json.message);
+    }
+    if (responce.ok) {
+      // Reset all states for form
+      setError(null);
+      console.log("New sleep item added");
+    }
+  };
   return (
     <div>
       <Button onClick={handleShow}>Sleep Form</Button>
@@ -19,6 +41,8 @@ const SleepForm = () => {
           <Modal.Title>Sleep Form</Modal.Title>
         </Modal.Header>
         <Modal.Body>
+          {/* Add onChange={(e)=. setState(e.target.value)} for each form part */}
+          {/* Also add value={value} to each Form.Control/look up react bootstrap stuff */}
           <Form>
             <Form.Group className="mb-3" controlId="date">
               <Form.Label>Date of Sleep</Form.Label>
